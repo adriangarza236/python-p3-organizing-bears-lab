dab0a08ed4f55c5a004de77de58be40728b30d56
Adrian Garza <adriangarza236@yahoo.com>
2025-01-31 08:15:34 -0600

    completed bears lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO  bears (name, age, sex, color, temperament, alive) VALUES ("Mr.Chocolate", 5, "M", "black", "nice", 1); 
+INSERT INTO  bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 12, "F", "brown", "angry", 1);
+INSERT INTO  bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 3, "F", "white", "even", 0);
+INSERT INTO  bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 4, "M", "black", "upset", 1);
+INSERT INTO  bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 6, "M", "brown", "sad", 1);
+INSERT INTO  bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 9, "M", "black", "angry", 0);
+INSERT INTO  bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 12, "F", "white", "nice", 1);
+INSERT INTO  bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3, "M", "brown", "angry", 0); 
